8ea6ba2bf8c6443f1f9a38350795ce0eecd3df62
kaihulme <kaihulme97@gmail.com>
2020-10-20 20:24:30 +0100

    agent_ask_oracle

--- grid_world/lab_identity_12345.pl
+++ grid_world/lab_identity_12345.pl
@@ -1,9 +1,17 @@
+% clear screen
+clear :- write('\33\[2J').
+
 % This is the main predicate, and should be true only when A is your identity
 % find_identity(-A)
-find_identity(A) :-
-  true.
+find_identity(A) :- actor(A).
 
 % This is a helper predicate and should find all the links for a particular actor
 % find_links_by_actor(+A,-L)
-find_links_by_actor(A,L) :-
-  true.
+find_links_by_actor(A,L) :- wp(A, WikiText),
+                            wt_link(WikiText, L).
+
+find_secret_actor(A) :- actor(A),
+                        wp(A, WikiText),
+                        wt_link(WikiText, L),
+                        agent_ask_oracle(A, o(1), L, Ans),
+                        Ans \= 'I do not know' .
