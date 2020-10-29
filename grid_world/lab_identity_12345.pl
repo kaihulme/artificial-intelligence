cd2c7296731e5b6c446fe4d0865a88e85db7540c
kaihulme <kaihulme97@gmail.com>
2020-10-29 17:23:47 +0000

    Genetic algorithm tournament analysis

--- grid_world/lab_identity_12345.pl
+++ grid_world/lab_identity_12345.pl
@@ -3,7 +3,7 @@ clear :- write('\33\[2J').
 
 % This is the main predicate, and should be true only when A is your identity
 % find_identity(-A)
-find_identity(A) :- actor(A).
+find_identity(A) :- actor(A) .
 
 % This is a helper predicate and should find all the links for a particular actor
 % find_links_by_actor(+A,-L)
@@ -15,3 +15,5 @@ find_secret_actor(A) :- actor(A),
                         wt_link(WikiText, L),
                         agent_ask_oracle(A, o(1), L, Ans),
                         Ans \= 'I do not know' .
+
+
