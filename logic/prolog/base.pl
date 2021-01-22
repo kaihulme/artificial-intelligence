f1c7690fcfcfb1170093c96883f50dabdd0b37ca
kaihulme <kaihulme97@gmail.com>
2021-01-22 16:45:43 +0000

    LPN 2

---
+++ logic/prolog/base.pl
@@ -0,0 +1,4 @@
+%%% BASE HELPER FUNCTIONS %%%
+
+% clear screen
+clear :- write('\33\[2J').
--- logic/prolog/lpn_02.pl
+++ logic/prolog/lpn_02.pl
@@ -20,21 +20,30 @@
 %% meal(food(bread), drink(beer)) = meal(X, Y)
 %% meal(food(bread), X) = meal(X, drink(beer))
 
-%
-%
-%
-%
-%
+% true
+% false
+% true
+% Bread = bread
+% false
+% false
+% X = food(bread)
+% X = bread
+% X = sausage, Y = bread
+% false
+% false
+% X = food(X)
+% X = food(bread), Y = drink(beer)
+% false
 
 %% 2.2: We are working with the following knowledge base.
 
 house_elf(dobby) .
 witch(hermione) .
-witch(’McGonagall’) .
+witch('McGonagall') .
 witch(rita_skeeter) .
-magic(X) :- house_elf(X) .
-magic(X) :- wizard(X) .
-magic(X) :- witch(X) .
+magic(X):- house_elf(X) .
+magic(X):- wizard(X) .
+magic(X):- witch(X) .
 
 %% Which of the following queries are satisfied? Where relevant, give all the 
 %% variable instantiations that lead to success.
@@ -45,22 +54,27 @@ magic(X) :- witch(X) .
 %% ?- magic(’McGonagall’) .
 %% ?- magic(Hermione) .
 
-%
-%
-%
-%
-%
+% false
+% false
+% false
+% true
+% Hermione = dobby, Hermione = hermione, ...
+% ... Hermione = 'McGonagall', Hermione = rita_skeeter
 
 %% Draw the search tree for the query:
 
 %% magic(Hermione) .
 
-%
-%
-%
-%
-%
-%
+% ?- magic(Hermione).
+% |
+% +------------------------+----------------------.
+% |                        |                      |
+% ?- house_elf(Hermione).  ?- wizard(Hermione).   ?- witch(Hermione).
+% |                        |                      |
+% +-------------------.    #                      +----------------------+--------------------------+--------------------------.
+% |                   |                           |                      |                          |                          |
+% []                  #                           []                     []                         []                         #
+% {Hermione = dobby}                              {Hermione = hermione}  {Hermione = 'McGonagall'}  {Hermione = rita_skeeter}
 
 %% 2.3: Here is a tiny lexicon (that is, information about individual words) 
 %% and  a mini grammar consisting of one syntactic rule (which defines a 
@@ -70,7 +84,7 @@ magic(X) :- witch(X) .
 word(determiner, a) .
 word(determiner, every) .
 word(noun, criminal).
-word(noun, ’big kahuna burger’) .
+word(noun, 'big kahuna burger') .
 word(verb, eats) .
 word(verb, likes) .
 
@@ -84,10 +98,8 @@ sentence(Word1, Word2, Word3, Word4, Word5):- word(determiner, Word1) ,
 %% grammar can generate? List all sentences that this grammar can generate in 
 %% the order that Prolog will generate them in.
 
-%
-%
-%
-%
+% sentence(W1, W2, W3, W4, W5), 
+% format("~w ~w ~w ~w ~w", [W1, W2, W3, W4, W5]).
 
 %% 2.4: Here are six Italian words: astante, astoria, baratto, cobalto, pistola,
 %% statale. They are to be arranged, crossword puzzle fashion 
@@ -105,8 +117,28 @@ word(statale, s, t, a, t, a, l, e) .
 %% first three  arguments should be the vertical words from left to right, and 
 %% the last three arguments the horizontal words from top to bottom.
 
-%
-%
-%
-%
-%
+crossword(V1, V2, V3, H1, H2, H3):- 
+
+    word(V1, V11, V12H12, V13, V14H22, V15, V16H32, V17) ,
+    word(V2, V21, V22H14, V23, V24H24, V25, V26H34, V27) ,
+    word(V3, V31, V32H16, V33, V34H26, V35, V36H36, V37) ,
+    word(H1, H11, V12H12, H13, V22H14, H15, V32H16, H17) ,
+    word(H2, H21, V14H22, H23, V24H24, H25, V34H26, H27) ,
+    word(H3, H31, V16H32, H33, V26H34, H35, V36H36, H37) ,
+    \+ member(V1, [V2, V3, H1, H2, H3]) ,
+
+    format("    .---.   .---.   .---.\n") , flush_output ,
+    format("    | ~w |   | ~w |   | ~w |    \n", [V11, V21, V31]) , flush_output ,
+    format(".---+---+---+---+---+---+---.\n") , flush_output ,
+    format("| ~w | ~w | ~w | ~w | ~w | ~w | ~w |\n", [H11, V12H12, H13, V22H14, H15, V32H16, H17]) , flush_output ,
+    format("'---+---+---+---+---+---+---'\n") , flush_output ,
+    format("    | ~w |   | ~w |   | ~w |    \n", [V13, V23, V33]) , flush_output ,
+    format(".---+---+---+---+---+---+---.\n") , flush_output ,
+    format("| ~w | ~w | ~w | ~w | ~w | ~w | ~w |\n", [H21, V14H22, H23, V24H24, H25, V34H26, H27]) , flush_output ,
+    format("'---+---+---+---+---+---+---'\n") , flush_output ,
+    format("    | ~w |   | ~w |   | ~w |    \n", [V15, V25, V35]) , flush_output ,
+    format(".---+---+---+---+---+---+---.\n") , flush_output ,
+    format("| ~w | ~w | ~w | ~w | ~w | ~w | ~w |\n", [H31, V16H32, H33, V26H34, H35, V36H36, H37]) , flush_output ,
+    format("'---+---+---+---+---+---+---'\n") , flush_output ,
+    format("    | ~w |   | ~w |   | ~w |    \n", [V17, V27, V37]) ,flush_output ,
+    format("    '---'   '---'   '---'\n") , flush_output .
